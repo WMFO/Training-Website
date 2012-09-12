925882aad4ffc741afa2b99353c198dc1b529378
Nicholas Andre <nick@nicholasandre.com>
2012-09-12 00:01:17 -0400

    timezone test

--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('America/New_York');
 require_once("./includes/session_timeout.inc.php");
 require("./includes/connection.inc.php");
 if (isset($_POST['settingsubmit'])){

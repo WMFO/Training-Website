9ebab309753a5ade2f897a72e134da7d8a2392b9
Nicholas Andre <nick@nicholasandre.com>
2012-09-15 21:14:10 -0400

    adding css stuff

---
+++ head.inc.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 3.0 License
+
+Name       : Resolved
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20120205
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<title>Training Portal</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="header">
+		<div id="logo">
+			<h1><a href="#">Training</a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="first current_page_item"><a href="#">portal</a></li>
+				<li><a href="#">Edit Info</a></li>
+				<li><a href="#">Password Reset</a></li>
+				<li><a href="#">About</a></li>
+				<li class="last"><a href="#">logout</a></li>
+			</ul>
+			<br class="clearfix" />
+		</div>
+	</div>
+	<div id="page">
+		<div id="content">
+			<div id="post1">
---
+++ tail.inc.php
@@ -0,0 +1,30 @@
+                        <br class="clearfix" />
+                </div>
+
+</div>
+		<div id="sidebar">
+			<h3>important dates</h3>
+			<div class="date-list">
+				<ul class="list date-list">
+					<li class="first"><span class="date">Sep 20</span> - Registration Opens at Noon</li>
+					<li><span class="date">Sep 21 </span>- Registration Closes at 11:59:59</li>
+					<li><span class="date">Sep 24</span> - Training Begins </li>
+					<li><span class="date">Oct 7</span> - Training Ends </li>
+					<li class="last"><span class="date">Oct 10</span> - Show Forms Due </li>
+				</ul>
+			</div>
+			<h3>wmfo training system</h3>
+			<p>
+                        The WMFO Training Education Management System (the TEMS) was built and maintained by Nicholas Andre.
+</p>
+<p><i> It's like SIS, but not terrible.	</i>
+			</p>
+		</div>
+		<br class="clearfix" />
+	</div>
+</div>
+<div id="footer">
+	(c) 2012 Nicholas Andre. Design by <a href="http://www.freecsstemplates.org/">FCT</a>.
+</div>
+</body>
+</html>

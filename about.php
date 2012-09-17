7bccd2c498059d611c3c2b50e8f62d0b75fbb442
Nicholas Andre <nick@nicholasandre.com>
2012-09-16 20:16:58 -0400

    about update

--- about.php
+++ about.php
@@ -1,12 +1,9 @@
 <?php include('./head.inc.php');?>
 <h1>About the Training Education Management System</h1>
-<table width="700px">
-<tr><td>
 <p>The WMFO Training Education Management System is a platform constructed by Nicholas Andre in the Summer of 2012 and first used during the Fall 2012 training at WMFO. The goals of the project were to consolidate the infrastructure of training to a central location, keeping track of registration and attendance in one place. Furthermore, a checklist of items is provided within the system to ensure that the training process maintains consistency from one trainer to another.</p>
 <p>Furthermore, the system is designed to be managed by personel with no knowledge of the underlying codebase. All administrative functions can be performed without access to the underlying architecture, providing a more stable and reusable platform.</p>
 <p>The system was also designed with a prudent level of security in mind, storing passwords in a safe way without the original password. The system also requires long passwords to ensure that everyone's account is secure.</p>
 <p>While the system is in its early trials, please report any bugs or errors encountered to the address at the bottom of the page. Doing this will help make the platform better, and ensure that everyone obtains their training in a safe and rapid manner.</p>
+<p>In version 1.5, tems received a facelift. Before it was just boring black and white text.</p>
 <font color="grey"><i>&copy;<?php echo date("Y");?> Nicholas Andre </i></font>
-</tr></td>
-</table>
 <?php include('./tail.inc.php'); ?>

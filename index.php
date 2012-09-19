fa7b79ff74a25c020fc95cbfaaed52e8797639e5
Nicholas Andre <nick@nicholasandre.com>
2012-09-19 13:39:20 -0400

    message for trainees

--- index.php
+++ index.php
@@ -24,6 +24,11 @@ Welcome,
 <?php echo $_SESSION['fname']; ?>
 !
 </p>
+<?php if(1) {?>
+<table border="2" width="400px">
+<tr><td style="background-color: #FFCACA"><b>A Note:</b> We have a VERY heavy load of trainees this semester. You stand a much better chance of getting a good show (or any show at all if we have 70+ people request a show) if you apply with 1 or 2 or 3 friends and share a show. Also, coming to volunteer days is highly recommended to get a higher ranking.</td></tr>
+</table><br />
+<?php } ?>
 <?php if (!$register) {?><p>Your registration and login is successful. Please log back in at the appropriate time to select your show.</p>
 <?php } else { ?>
 <p>The WMFO training portal.</p>
@@ -41,11 +46,6 @@ Welcome,
 <?php echo $_SESSION['fname']; ?>
 !
 </p>
-<?php if(0) {?>
-<table border="2" width="400px">
-<tr><td style="background-color: yellow"><b>Notice:</b> WMFO is transitioning to a two (2) week training time frame to enhance overall efficiency of the training process. The Fall 2012 training will be a trial run of the new system. To facilitate the shorter timeframe, we will also be using the TEMS, a training management infrastructure for keeping track of attendance and training goals. If you have any questions, feel free to ask via email.</td></tr>
-</table>
-<?php } ?>
 <p>You can view DJ names and contact info below once they join your show. Please take attendance and fill out the checklist every week during your show!</p>
 
 <?php include('./includes/trainer_startpage.inc.php'); ?>

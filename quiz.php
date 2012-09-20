cac4e81ef22f1d092a34bcfb2e8971a29fb228cf
Nicholas Andre <nick@nicholasandre.com>
2012-09-20 10:34:53 -0400

    quiz page mods

--- quiz.php
+++ quiz.php
@@ -4,50 +4,19 @@ require_once("./includes/session_timeout.inc.php");
 require("./includes/connection.inc.php");
 $conn=dbConnect('write');
 $role = $_SESSION['role'];
-if (isset($_POST['reorder']) && $role == "admin") {
-  require("./includes/checkorder.inc.php");
-}
-$sql = "SELECT * FROM checklist ORDER BY onum ASC";
+$sql = "SELECT * FROM quiz_questions ORDER BY qnum ASC";
 $result = $conn->query($sql);
 include('./head.inc.php');
 ?>
-<html>
-<head>
-<title="Checklis">
-</head>
-<body>
-<h1>Checklist Management</h1>
-<p>Here you will find a list of the checklist items. You may freely add, remove, rearrange, etc.</p>
-<p>The order number is sorted ascending, so to change the order simply change the listed numbers and then hit update below. Modification and delete links are to the right of each item, while the add new item link is at the bottom. Enjoy!</p>
-<?php if ($role == "admin") {?>
-  <form method="post" action="" name="reorder">
-<?php } else {?>
-<p>This page details the checklist items for all weeks. Make sure you've covered all of these if you're doing a make up lesson.</p>
+<h1>Quiz</h1>
+<p>Before you can register for a show, you must complete this quiz. It is designed to assess your knowledge of basic operating practices here at the station. If you do poorly on the quiz, we'll ask for you to attend a final session where we'll go over additional details.</p>
+<form method="post" action="" name="quizform">
+<?php
+while($row = $result->fetch_assoc()) { ?>
+<p>Question <?php echo $row['qnum'] ?>:</p>
+<?php echo $row['content'];
+echo $row['input'];?>
 <?php } ?>
-<table border="2">
-<tr>
-<th>Weeknumber</th>
-<?php if ($role == "admin") { echo "<th>Order Number</th>";}?>
-<th>Item</th>
-<?php if ($role == "admin") { echo "<th>Modify</th>";}?>
-</tr>
-<?php while($row = $result->fetch_assoc()) { ?>
-<tr>
-<td><?php echo $row['weeknum']; ?></td>
-<?php if ($role == "admin") { ?>
-<td><input type="text" size="3" name="<?php echo $row['id'];?>"
-value="<?php echo $row['onum']; ?>"></td>
-<?php } ?>
-<td><?php echo $row['item'];?></td>
-<?php if ($role == "admin") {?>
-<td><a href="modcheck.php?item=<?php echo $row['id']; ?>">Modify</a> <a href="delcheck.php?item=<?php echo $row['id']; ?>">Delete</a></td>
-<?php } ?>
-</tr>
-<?php } ?>
-</table>
-<?php if ($role == "admin") {?>
-<p><input type="submit" name="reorder" value="Change Order"></p>
+<input type="submit" name="submitquiz" value="Submit Answers">
 </form>
-<p><a href="modcheck.php">Add New Item</a></p>
-<?php } 
-include('./tail.inc.php');?>
+<?php include('./tail.inc.php');?>

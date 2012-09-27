3c5840053e1011dc68026be2c78b2bc45160c1c1
Nicholas Andre <nick@nicholasandre.com>
2012-09-27 19:18:56 -0400

    fix

--- quizmod.php
+++ quizmod.php
@@ -3,7 +3,7 @@ include('./includes/logout.inc.php');
 require_once("./includes/session_timeout.inc.php");
 require("./includes/connection.inc.php");
 $conn=dbConnect('write');
-if ($role != "admin") {
+if ($_SESSION['role'] != "admin") {
   header("Location: index.php");
 }
 $sql = "SELECT * FROM quiz_questions ORDER BY qnum ASC";

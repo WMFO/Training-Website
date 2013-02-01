46f092ad1bfa4e30d2d4acc731ac88b73606a27a
Nicholas Andre <nick@nicholasandre.com>
2013-02-01 23:51:23 +0000

    allow admin to mark quiz as passed

--- quiz_person.php
+++ quiz_person.php
@@ -11,6 +11,10 @@ if (isset($_POST['resetviews']) && $_SESSION['role'] == "admin") {
     $sql2 = "DELETE FROM quiz_views WHERE id_fk = " . $person;
     $conn->query($sql2);
   }
+  foreach($_POST['markpass'] as $person) {
+    $sql2 = "UPDATE users SET quiz_score = 101 WHERE user_id = " . $person;
+    $conn->query($sql2);
+  }
 }
 $sql = "SELECT fname, lname, email, user_id, quizscore FROM quiz_views LEFT JOIN users on user_id = id_fk";
 $result = $conn->query($sql);
@@ -21,25 +25,26 @@ $result = $conn->query($sql);
 </head>
 <body>
 <h1>Quiz Management</h1>
-<p>Here you will find a list of the quiz items. You may freely add, remove, rearrange, etc.</p>
   <form method="post" action="" name="meh">
 <table border="2">
 <tr>
 <th>Person</th>
 <th>Email</th>
 <th>Quiz Score</th>
-<th>Reset</th>
+<th>Reset View</th>
+<th>Mark Person as Passed</th>
 </tr>
 <?php while($row = $result->fetch_assoc()) { ?>
 <tr>
 <td><?php echo $row['fname'] . ' ' . $row['lname']; ?></td>
 <td><?php echo $row['email'];?></td>
 <td><?php echo $row['quizscore'];?></td>
-<td><input type="checkbox" name="delview[]" value="<?php echo $row['user_id']; ?>"></td>
+<td>R<input type="checkbox" name="delview[]" value="<?php echo $row['user_id']; ?>"></td>
+<td>P<input type="checkbox" name="markpass[]" value="<?php echo $row['user_id']; ?>"></td>
 </tr>
 <?php } ?>
 </table>
-<input type="submit" name="resetviews" value="Reset Quiz Views">
+<input type="submit" name="resetviews" value="Submit">
 </form>
 <a href="index.php">Home</a>
 </body>

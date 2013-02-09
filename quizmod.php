1562b784d32772f125dc75ae29069c897b30baae
Nicholas Andre <nick@nicholasandre.com>
2013-02-09 21:46:22 +0000

    a little language swapped around regarding the quiz

--- quizmod.php
+++ quizmod.php
@@ -14,7 +14,7 @@ $result = $conn->query($sql);
 <title>Quiz Question Management</title>
 </head>
 <body>
-<h1>Quiz Management</h1>
+<h1>Quiz Question Management</h1>
 <p>Here you will find a list of the quiz items. You may freely add, remove, rearrange, etc.</p>
 <p>The order number is sorted ascending, so to change the order simply change the listed numbers and then hit update below. If you wish to swap question numbers, please change one number high (eg 999) and then swap. Modification and delete links are to the right of each item, while the add new item link is at the bottom. Enjoy!</p>
 <p><a href="quiz_person.php">User Specific Quiz Functions</a></p>
